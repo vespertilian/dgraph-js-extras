715f749df086a17175494b3c257b0676e7150c6f
Cameron Batt <vespertilian@gmail.com>
2018-02-21 17:36:16 +1100

    v0.1.5

--- lib/upsert-now/upsert-now.d.ts
+++ lib/upsert-now/upsert-now.d.ts
@@ -1,5 +1,6 @@
 import * as dgraph from 'dgraph-js';
-export declare function XUpsertNow(searchPredicates: string | string[], data: object, dgraphClient: dgraph.DgraphClient, _dgraph?: typeof dgraph): Promise<void>;
+export declare function XUpsertNow(searchPredicates: string | string[], data: object[], dgraphClient: dgraph.DgraphClient, _dgraph?: any): Promise<string[]>;
+export declare function XUpsertNow(searchPredicates: string | string[], data: object, dgraphClient: dgraph.DgraphClient, _dgraph?: any): Promise<string>;
 export declare function buildUpsertQuery(_searchPredicates: string | string[], node: object): {
     query: string;
     searchValues: string[];
