e8e0e14b8a89ece1050293890aa9b1560406e19e
Cameron Batt <vespertilian@gmail.com>
2018-10-17 12:48:04 +1100

    test(delete-json-now): add extra tests to verify that you can delete multiple does with the one comm

--- src/delete-json-now/delete-json-now.spec.ts
+++ src/delete-json-now/delete-json-now.spec.ts
@@ -2,6 +2,7 @@ import {xSetupWithSchemaDataNow} from '../test-helpers/setup';
 import {xExtractNamedUids} from '../extract-named-uid/extract-named-uids';
 import {xQueryNow} from '../query-with-vars-now/query-with-vars-now';
 import {xDeleteJSONNow} from './delete-json-now';
+
 const schema = `
     name: string @index(exact) .
     street: string @index(exact) .
@@ -12,7 +13,9 @@ const sampleData = {
   name: "cameron",
   addresses: [
     {uid: "_:address1", street: "william", postcode: 2000},
-    {uid: "_:address2", street: "george", postcode: 2001}
+    {uid: "_:address2", street: "george", postcode: 2001},
+    {uid: "_:address3", street: "hay", postcode: 2444},
+    {uid: "_:address4", street: "short", postcode: 2107}
   ]
 };
 
@@ -20,7 +23,7 @@ const queryUsers = (prefix: string) => ` {
   ${prefix}(func: has(name)) {
     uid
     name
-    addresses {
+    addresses(orderasc: street) {
       uid
       street
     }
@@ -37,26 +40,30 @@ const queryAddressByUid = (prefix, uid) => `{
 }`;
 
 const queryAllAddresses = (prefix) => `{
-  ${prefix}(func: has(street)) {
+  ${prefix}(func: has(street), orderasc: street) {
     uid,
     street
     postcode
   }
 }`;
 
-describe("delete-json-now node from another node", () => {
-  it("lets you delete-json-now on node from another node", async() => {
+describe("delete-json-now", () => {
+  it("lets you delete a node (address) and the link that node had to another node (user has addresses - delete link pointing to deleted address)", async() => {
     const {result, dgraphClient} = await xSetupWithSchemaDataNow({schema, data: sampleData});
 
-    const [userUid, address1Uid, address2Uid] = xExtractNamedUids(['user', 'address1', 'address2'],result);
+    const [userUid, address1Uid] = xExtractNamedUids(['user', 'address1', 'address2'],result);
 
+    // one user
     const {u} = await xQueryNow({query: queryUsers('u')}, dgraphClient);
     expect(u.length).toBe(1);
-    expect(u[0].addresses.length).toBe(2);
+    // with 4 addresses
+    expect(u[0].addresses.length).toBe(4);
 
+    // 4 total addresses in db
     const {allA} = await xQueryNow({query: queryAllAddresses('allA')}, dgraphClient);
-    expect(allA.length).toBe(2);
+    expect(allA.length).toBe(4);
 
+    // address1 is william street
     const {a} = await xQueryNow({query: queryAddressByUid('a', address1Uid)}, dgraphClient);
     expect(a[0].street).toEqual('william');
 
@@ -74,22 +81,99 @@ describe("delete-json-now node from another node", () => {
 
     await xDeleteJSONNow(delete_json, dgraphClient);
 
-    // checks
+    // still one user
     const {u2} = await xQueryNow({query: queryUsers('u2')}, dgraphClient);
     expect(u2.length).toBe(1);
-    expect(u2[0].addresses.length).toBe(1);
 
+    // 3 addresses now
+    expect(u2[0].addresses.length).toBe(3);
+
+    // george, hay and short are the addresses we have left
     expect(u2[0].addresses[0].street).toEqual('george');
-    expect(u2[0].addresses[0].uid).toEqual(address2Uid);
+    expect(u2[0].addresses[1].street).toEqual('hay');
+    expect(u2[0].addresses[2].street).toEqual('short');
 
+    // george, hay and short are the addresses we have in the db
     const {allA2} = await xQueryNow({query: queryAllAddresses('allA2')}, dgraphClient);
-    expect(allA2.length).toBe(1);
-    expect(allA2[0].street).toBe('george');
+    expect(allA2.length).toBe(3);
     expect(allA2[0].postcode).toBe(2001);
+    expect(allA2[0].street).toBe('george');
+    expect(allA2[1].postcode).toBe(2444);
+    expect(allA2[2].postcode).toBe(2107);
 
+    // we can no longer query for the old address
     const {a2} = await xQueryNow({query: queryAddressByUid('a2', address1Uid)}, dgraphClient);
     expect(a2[0].street).toBeUndefined();
     expect(a2[0].postcode).toBeUndefined();
+  });
 
+  it("lets you delete multiple nodes (2x addresses) and the links that those noes had to another node (user has addresses - delete two links pointing to addresses", async() => {
+    const {result, dgraphClient} = await xSetupWithSchemaDataNow({schema, data: sampleData});
+    const [userUid, address1Uid, address2Uid] = xExtractNamedUids(['user', 'address1', 'address2', 'address3', 'address4'],result);
+
+
+    // one user
+    const {u} = await xQueryNow({query: queryUsers('u')}, dgraphClient);
+    expect(u.length).toBe(1);
+    // with 4 addresses
+    expect(u[0].addresses.length).toBe(4);
+
+    // 4 total addresses in db
+    const {allA} = await xQueryNow({query: queryAllAddresses('allA')}, dgraphClient);
+    expect(allA.length).toBe(4);
+
+    // address1 is william street
+    const {a} = await xQueryNow({query: queryAddressByUid('a', address1Uid)}, dgraphClient);
+    expect(a[0].street).toEqual('william');
+
+    // address2 is george
+    const {a2} = await xQueryNow({query: queryAddressByUid('a2', address2Uid)}, dgraphClient);
+    expect(a2[0].street).toEqual('george');
+
+    const delete_json = [
+      {
+        uid: userUid, // user uid
+        addresses: [
+          { uid: address1Uid }, // delete link with address 1
+          { uid: address2Uid }, // delete link with address 2
+        ],
+      },
+      {
+        uid: address1Uid // delete actual address 1
+      },
+      {
+        uid: address2Uid // delete actual address 2
+      }
+    ];
+
+    await xDeleteJSONNow(delete_json, dgraphClient);
+
+    // still one user
+    const {u2} = await xQueryNow({query: queryUsers('u2')}, dgraphClient);
+    expect(u2.length).toBe(1);
+
+    // 2 addresses now
+    expect(u2[0].addresses.length).toBe(2);
+
+    // hay and short are the addresses we have left
+    expect(u2[0].addresses[0].street).toEqual('hay');
+    expect(u2[0].addresses[1].street).toEqual('short');
+
+    // hay and short are the only addresses we have left in the db
+    const {allA2} = await xQueryNow({query: queryAllAddresses('allA2')}, dgraphClient);
+    expect(allA2.length).toBe(2);
+    expect(allA2[0].postcode).toBe(2444);
+    expect(allA2[0].street).toBe('hay');
+    expect(allA2[1].postcode).toBe(2107);
+    expect(allA2[1].street).toBe('short');
+
+    // we can no longer query for the old address
+    const {a3} = await xQueryNow({query: queryAddressByUid('a3', address1Uid)}, dgraphClient);
+    expect(a3[0].street).toBeUndefined();
+    expect(a3[0].postcode).toBeUndefined();
+
+    const {a4} = await xQueryNow({query: queryAddressByUid('a4', address2Uid)}, dgraphClient);
+    expect(a4[0].street).toBeUndefined();
+    expect(a4[0].postcode).toBeUndefined();
   })
 });
