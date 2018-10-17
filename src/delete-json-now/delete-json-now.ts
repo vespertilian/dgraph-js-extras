6601f9e16ef5f88d1f695c349e99d9e2d8a875ff
Cameron Batt <vespertilian@gmail.com>
2018-10-17 13:01:02 +1100

    feat(query-with-vars-now & delete-json-now): split these functions up so there is a function that ju

--- src/delete-json-now/delete-json-now.ts
+++ src/delete-json-now/delete-json-now.ts
@@ -1,9 +1,15 @@
 import * as dgraph from 'dgraph-js'
 import * as messages from "dgraph-js/generated/api_pb";
+import {Mutation} from 'dgraph-js';
 
-export async function xDeleteJSONNow(json: object, dgraphClient: dgraph.DgraphClient, _dgraph=dgraph): Promise<messages.Assigned> {
+export function xDeleteJSON(json, _dgraph=dgraph): Mutation {
   const mu = new _dgraph.Mutation();
   mu.setDeleteJson(json);
+  return mu
+}
+
+export async function xDeleteJSONNow(json: object, dgraphClient: dgraph.DgraphClient): Promise<messages.Assigned> {
+  const mu = xDeleteJSON(json);
   mu.setCommitNow(true);
-  return dgraphClient.newTxn().mutate(mu)
+  return dgraphClient.newTxn().mutate(mu);
 }
--- src/query-with-vars-now/query-with-vars-now.ts
+++ src/query-with-vars-now/query-with-vars-now.ts
@@ -1,11 +1,17 @@
 import * as dgraph from 'dgraph-js'
+import {Txn} from 'dgraph-js';
 
 export interface IxQueryParams {
   query: string;
   vars?: {[k: string]: any}
 }
 
-export async function xQueryNow({query, vars}: IxQueryParams, dgraphClient: dgraph.DgraphClient) {
-  const res = await dgraphClient.newTxn().queryWithVars(query, vars);
+export async function xQueryWithVars({query, vars}: IxQueryParams, txn: Txn) {
+  const res = await txn.queryWithVars(query, vars);
   return res.getJson();
+}
+
+export async function xQueryNow({query, vars}: IxQueryParams, dgraphClient: dgraph.DgraphClient) {
+  const txn = dgraphClient.newTxn();
+  return await xQueryWithVars({query, vars}, txn);
 }
